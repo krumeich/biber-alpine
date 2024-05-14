724b4c2673ed3b5ad02f8b27a5bdcd9a1b4a5f69
Alexander Krumeich <alexander.krumeich@n-design.de>
2024-05-14 16:41:25 +0200

    Compress binary for CTAN

--- biberpackage.sh
+++ biberpackage.sh
@@ -4,11 +4,11 @@ BIBER_VERSION=$(docker run --rm -v $(pwd):/usr/local/bin alpine /usr/local/bin/b
 PRODUCT=biber
 METAPLATFORM=linux-musl
 METAPLATFORM_DIR=${PRODUCT}-${METAPLATFORM}
-ARCHIVE=${PRODUCT}-${BIBER_VERSION}-${METAPLATFORM}_x86_64.tar
+ARCHIVE=${PRODUCT}-${BIBER_VERSION}-${METAPLATFORM}_x86_64.tar.gz
 
 rm -rf ${METAPLATFORM_DIR}
 mkdir -p ${METAPLATFORM_DIR}
-tar cf ${METAPLATFORM_DIR}/${ARCHIVE} biber
+tar czf ${METAPLATFORM_DIR}/${ARCHIVE} biber
 
 cat >${METAPLATFORM_DIR}/README <<EOF
 These are biber binaries for the ${METAPLATFORM} platform.
